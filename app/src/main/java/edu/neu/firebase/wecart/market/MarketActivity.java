7b930b502dac74b7490e1d352e364d17c599226e
Jiapei1001 <jiapeili@live.com>
2021-11-27 20:46:11 -0800

    set up scroller view

--- app/src/main/java/edu/neu/firebase/wecart/market/MarketActivity.java
+++ app/src/main/java/edu/neu/firebase/wecart/market/MarketActivity.java
@@ -16,12 +16,12 @@ public class MarketActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_market);
 
-        ScrollView scrollView = (ScrollView) findViewById(R.id.market01ScrollView);
-        HorizontalScrollView scrollViewHorizontal = (HorizontalScrollView) findViewById(R.id.market01HorizontalScrollView);
-//        scrollView.smoothScrollTo(2,3);
-//        scrollView.smoothScrollBy(1, 2);
-
-        ObjectAnimator.ofInt(scrollView, "scrollY",  scrollView.getBottom()).setDuration(6000).start();
-        ObjectAnimator.ofInt(scrollViewHorizontal, "scrollX",  scrollViewHorizontal.getRight()).setDuration(6000).start();
+//        ScrollView scrollView = (ScrollView) findViewById(R.id.market01ScrollView);
+//        HorizontalScrollView scrollViewHorizontal = (HorizontalScrollView) findViewById(R.id.market01HorizontalScrollView);
+////        scrollView.smoothScrollTo(2,3);
+////        scrollView.smoothScrollBy(1, 2);
+//
+//        ObjectAnimator.ofInt(scrollView, "scrollY",  scrollView.getBottom()).setDuration(6000).start();
+//        ObjectAnimator.ofInt(scrollViewHorizontal, "scrollX",  scrollViewHorizontal.getRight()).setDuration(6000).start();
     }
 }
