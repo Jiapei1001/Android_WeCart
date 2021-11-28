865c3f2f51993c59532bbcf7c3959dbf9d140170
Jiapei1001 <jiapeili@live.com>
2021-11-27 17:33:10 -0800

    initiate store activity

---
+++ app/src/main/java/edu/neu/firebase/wecart/market/StoreActivity.java
@@ -0,0 +1,16 @@
+package edu.neu.firebase.wecart.market;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.neu.firebase.wecart.R;
+
+public class StoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store);
+    }
+}
