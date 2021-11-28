e1a00929d70c49050247d94f92de8c291375d7b9
Jiapei1001 <jiapeili@live.com>
2021-11-27 22:17:59 -0800

    Add Scroller View for Market & its Background (#4)
    * initial start of market activity
    
    * smooth scrolling
    
    * initiate store activity
    
    * set up scroller view

--- app/src/main/java/edu/neu/firebase/wecart/MainActivity.java
+++ app/src/main/java/edu/neu/firebase/wecart/MainActivity.java
@@ -2,7 +2,12 @@ package edu.neu.firebase.wecart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import edu.neu.firebase.wecart.market.MarketActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // temporary placeholder for opening market activity
+        Button marketBtn = findViewById(R.id.marketBtn);
+        marketBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MarketActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
