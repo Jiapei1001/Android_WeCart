a26ca6211865d7a2cbf128a3e60660f065b787a3
Jiapei1001 <jiapeili@live.com>
2021-12-04 09:17:44 -0800

    add market default constructor

--- app/src/main/java/edu/neu/firebase/wecart/market/Market.java
+++ app/src/main/java/edu/neu/firebase/wecart/market/Market.java
@@ -12,6 +12,23 @@ public class Market {
     String email;
     String phone;
 
+    public Market() {
+        // default constructor
+    }
+
+    public Market(int id, String storeName, String storeBtn, String storeImage, String storeDes, Double latitude, Double longitude, String address, String email, String phone) {
+        this.id = id;
+        this.storeName = storeName;
+        this.storeBtn = storeBtn;
+        this.storeImage = storeImage;
+        this.storeDes = storeDes;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.address = address;
+        this.email = email;
+        this.phone = phone;
+    }
+
 
 
 }
