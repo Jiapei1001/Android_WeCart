ab06ce2b2d30541a080d0a7e810a8cce809cfaad
Jiapei1001 <jiapeili@live.com>
2021-12-04 18:20:35 -0800

    edit store btn in market to start store activity

--- app/src/main/java/edu/neu/firebase/wecart/market/MarketActivity.java
+++ app/src/main/java/edu/neu/firebase/wecart/market/MarketActivity.java
@@ -3,7 +3,12 @@ package edu.neu.firebase.wecart.market;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.ObjectAnimator;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.HorizontalScrollView;
 import android.widget.ScrollView;
 
@@ -23,5 +28,29 @@ public class MarketActivity extends AppCompatActivity {
 //
 //        ObjectAnimator.ofInt(scrollView, "scrollY",  scrollView.getBottom()).setDuration(6000).start();
 //        ObjectAnimator.ofInt(scrollViewHorizontal, "scrollX",  scrollViewHorizontal.getRight()).setDuration(6000).start();
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.store1:
+                String storeId = idToString(R.id.store1);
+                openClickyActivity(storeId);
+                break;
+        }
+    }
+
+    public void openClickyActivity(String storeId) {
+        Intent intent = new Intent(this, StoreActivity.class);
+        intent.putExtra("storeId", storeId);
+        startActivity(intent);
+    }
+
+    private String idToString(int id) {
+        @SuppressLint("ResourceType") String storeName = getString(id);
+        System.out.println(storeName);
+        String storeId = storeName.replace("store", "");
+        System.out.println(storeId);
+
+        return storeId;
     }
 }
