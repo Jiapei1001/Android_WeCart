3bac83a9cd8a9a27bf1dd4131860136a8f1f0fe1
Jiapei1001 <jiapeili@live.com>
2021-12-04 18:48:13 -0800

    fix intent put extra to get store id

--- app/src/main/java/edu/neu/firebase/wecart/market/StoreActivity.java
+++ app/src/main/java/edu/neu/firebase/wecart/market/StoreActivity.java
@@ -2,15 +2,21 @@ package edu.neu.firebase.wecart.market;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import edu.neu.firebase.wecart.R;
 
 public class StoreActivity extends AppCompatActivity {
 
+    String storeId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store);
+
+        String storeId = getIntent().getStringExtra("storeId");
+        System.out.println(storeId);
     }
 }
