2d6448468bb2067efbf97ed36df8a6bd3055cce1
Jiapei1001 <jiapeili@live.com>
2021-12-05 19:41:47 -0800

    format

--- app/src/main/java/edu/neu/firebase/wecart/market/StoreActivity.java
+++ app/src/main/java/edu/neu/firebase/wecart/market/StoreActivity.java
@@ -1,14 +1,11 @@
 package edu.neu.firebase.wecart.market;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.google.firebase.database.DataSnapshot;
