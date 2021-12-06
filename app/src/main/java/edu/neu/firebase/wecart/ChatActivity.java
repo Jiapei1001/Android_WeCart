37284b89fc4a9712c8f3b4bd68caa0834ee4f723
Yu Fu <fu.yu4@northeastern.edu>
2021-12-05 16:53:48 -0800

    Changes on chat

--- app/src/main/java/edu/neu/firebase/wecart/ChatActivity.java
+++ app/src/main/java/edu/neu/firebase/wecart/ChatActivity.java
@@ -16,8 +16,6 @@ public class ChatActivity extends AppCompatActivity {
     private List<Chat_message> chat_message_list = new ArrayList<>();
     private RecyclerView msgRecyclerView;
     private EditText chat_user_content;
-    private Button send_message;
-    private LinearLayoutManager layoutManager;
     private MsgAdapter adapter;
 
     @Override
@@ -27,40 +25,37 @@ public class ChatActivity extends AppCompatActivity {
 
         msgRecyclerView = findViewById(R.id.msg_recycler_view);
         chat_user_content = findViewById(R.id.input_text);
-        send_message = findViewById(R.id.send_message);
-        layoutManager = new LinearLayoutManager(this);
+        Button send_message = findViewById(R.id.send_message);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         adapter = new MsgAdapter(chat_message_list = getData());
 
         msgRecyclerView.setLayoutManager(layoutManager);
         msgRecyclerView.setAdapter(adapter);
 
-        send_message.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String content = chat_user_content.getText().toString();
-                //set response when input is empty
-                if(!content.equals("")) {
-                    chat_message_list.add(new Chat_message(content, Chat_message.TYPE_send_message));
-                    adapter.notifyItemInserted(chat_message_list.size()-1);
-                    msgRecyclerView.scrollToPosition(chat_message_list.size()-1);
-                    chat_user_content.setText("");//Clear the input content
-                }
+        send_message.setOnClickListener(v -> {
+            String content = chat_user_content.getText().toString();
+            //set response when input is empty
+            if(!content.equals("")) {
+                chat_message_list.add(new Chat_message(content, Chat_message.TYPE_send_message));
+                adapter.notifyItemInserted(chat_message_list.size()-1);
+                msgRecyclerView.scrollToPosition(chat_message_list.size()-1);
+                chat_user_content.setText("");//Clear the input content
+            }
 //sample chat message list
-                if(chat_message_list.size() == 2){
-                    chat_message_list.add(new Chat_message("What's your name?", Chat_message.TYPE_RECEIVED));
-                    adapter.notifyItemInserted(chat_message_list.size()-1);
-                    msgRecyclerView.scrollToPosition(chat_message_list.size()-1);
-                }
-                if(chat_message_list.size() == 4){
-                    chat_message_list.add(new Chat_message("Any other questions?", Chat_message.TYPE_RECEIVED));
-                    adapter.notifyItemInserted(chat_message_list.size()-1);
-                    msgRecyclerView.scrollToPosition(chat_message_list.size()-1);
-                }
-                if(chat_message_list.size() == 6){
-                    chat_message_list.add(new Chat_message("Question recorded, bye!", Chat_message.TYPE_RECEIVED));
-                    adapter.notifyItemInserted(chat_message_list.size()-1);
-                    msgRecyclerView.scrollToPosition(chat_message_list.size()-1);
-                }
+            if(chat_message_list.size() == 2){
+                chat_message_list.add(new Chat_message("What's your name?", Chat_message.TYPE_RECEIVED));
+                adapter.notifyItemInserted(chat_message_list.size()-1);
+                msgRecyclerView.scrollToPosition(chat_message_list.size()-1);
+            }
+            if(chat_message_list.size() == 4){
+                chat_message_list.add(new Chat_message("Any other questions?", Chat_message.TYPE_RECEIVED));
+                adapter.notifyItemInserted(chat_message_list.size()-1);
+                msgRecyclerView.scrollToPosition(chat_message_list.size()-1);
+            }
+            if(chat_message_list.size() == 6){
+                chat_message_list.add(new Chat_message("Question recorded, bye!", Chat_message.TYPE_RECEIVED));
+                adapter.notifyItemInserted(chat_message_list.size()-1);
+                msgRecyclerView.scrollToPosition(chat_message_list.size()-1);
             }
         });
     }
--- app/src/main/java/edu/neu/firebase/wecart/SellerChatActivity.java
+++ app/src/main/java/edu/neu/firebase/wecart/SellerChatActivity.java
@@ -16,8 +16,6 @@ public class SellerChatActivity extends AppCompatActivity {
     private List<Chat_message> chat_message_list = new ArrayList<>();
     private RecyclerView msgRecyclerView;
     private EditText chat_seller_content;
-    private Button send_message;
-    private LinearLayoutManager layoutManager;
     private MsgAdapter adapter;
 
     @Override
@@ -27,39 +25,36 @@ public class SellerChatActivity extends AppCompatActivity {
 
         msgRecyclerView = findViewById(R.id.msg_recycler_view);
         chat_seller_content = findViewById(R.id.input_text);
-        send_message = findViewById(R.id.send_message);
-        layoutManager = new LinearLayoutManager(this);
+        Button send_message = findViewById(R.id.send_message);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         adapter = new MsgAdapter(chat_message_list = getData());
 
         msgRecyclerView.setLayoutManager(layoutManager);
         msgRecyclerView.setAdapter(adapter);
 
-        send_message.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String content = chat_seller_content.getText().toString();
-                if(!content.equals("")) {
-                    chat_message_list.add(new Chat_message(content, Chat_message.TYPE_send_message));
-                    adapter.notifyItemInserted(chat_message_list.size()-1);
-                    msgRecyclerView.scrollToPosition(chat_message_list.size()-1);
-                    chat_seller_content.setText("");//Clear the input content
-                }
+        send_message.setOnClickListener(v -> {
+            String content = chat_seller_content.getText().toString();
+            if(!content.equals("")) {
+                chat_message_list.add(new Chat_message(content, Chat_message.TYPE_send_message));
+                adapter.notifyItemInserted(chat_message_list.size()-1);
+                msgRecyclerView.scrollToPosition(chat_message_list.size()-1);
+                chat_seller_content.setText("");//Clear the input content
+            }
 //sample chat
-                if(chat_message_list.size() == 2){
-                    chat_message_list.add(new Chat_message("I donno how to cancel order", Chat_message.TYPE_RECEIVED));
-                    adapter.notifyItemInserted(chat_message_list.size()-1);
-                    msgRecyclerView.scrollToPosition(chat_message_list.size()-1);
-                }
-                if(chat_message_list.size() == 4){
-                    chat_message_list.add(new Chat_message("Can you help me to cancel it?", Chat_message.TYPE_RECEIVED));
-                    adapter.notifyItemInserted(chat_message_list.size()-1);
-                    msgRecyclerView.scrollToPosition(chat_message_list.size()-1);
-                }
-                if(chat_message_list.size() == 6){
-                    chat_message_list.add(new Chat_message("I am ok now, thank you!", Chat_message.TYPE_RECEIVED));
-                    adapter.notifyItemInserted(chat_message_list.size()-1);
-                    msgRecyclerView.scrollToPosition(chat_message_list.size()-1);
-                }
+            if(chat_message_list.size() == 2){
+                chat_message_list.add(new Chat_message("I donno how to cancel order", Chat_message.TYPE_RECEIVED));
+                adapter.notifyItemInserted(chat_message_list.size()-1);
+                msgRecyclerView.scrollToPosition(chat_message_list.size()-1);
+            }
+            if(chat_message_list.size() == 4){
+                chat_message_list.add(new Chat_message("Can you help me to cancel it?", Chat_message.TYPE_RECEIVED));
+                adapter.notifyItemInserted(chat_message_list.size()-1);
+                msgRecyclerView.scrollToPosition(chat_message_list.size()-1);
+            }
+            if(chat_message_list.size() == 6){
+                chat_message_list.add(new Chat_message("I am ok now, thank you!", Chat_message.TYPE_RECEIVED));
+                adapter.notifyItemInserted(chat_message_list.size()-1);
+                msgRecyclerView.scrollToPosition(chat_message_list.size()-1);
             }
         });
     }
